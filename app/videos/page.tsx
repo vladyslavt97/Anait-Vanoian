e0561535b4bc24de2f1a119f7efaf29d816ef0e5
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-05-13 12:58:49 +0200

    Page in videos

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -1,8 +1,6 @@
-import Gallery from "@/components/Gallery"
-
 type Props = {}
 
-export default function page({}: Props) {
+export default function Page({}: Props) {
   return (
     <div className="bg-gradient-to-b from-white to-yellow-100/30 rounded-2xl m-2.5 p-4 h-screen flex flex-col justify-center gap-10">
         Videos incoming...
