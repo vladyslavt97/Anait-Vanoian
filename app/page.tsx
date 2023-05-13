b1318e546864ecb3096a0a4b1e441eed88c98904
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-05-13 12:30:28 +0200

    Bio name rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+"use client"
 import Bio from "@/components/Bio";
+import { useStore } from "@/components/State";
+
+interface languagesState {
+  language: string,
+}
 
 export default function Home() {
+  const language = useStore((state: languagesState) => state.language);
+    console.log('languagelanguage', language);
   return (
     <main className="bg-gradient-to-b from-white to-yellow-100/30 rounded-2xl m-2.5 p-4">
       <h1 className="font-bold text-center py-10 text-4xl italic bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-transparent">Kelemen Hanna</h1>
-      <h1 className="font-bold text-center py-2 text-lg italic bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-transparent">életrajz</h1>
+      {language === "hun" && <h1 className="font-bold text-center py-2 text-lg italic bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-transparent">életrajz</h1>}
+      {language === "eng" && <h1 className="font-bold text-center py-2 text-lg italic bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-transparent">biography</h1>}
       <Bio/>
     </main>
   )
