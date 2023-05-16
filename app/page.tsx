02c45b3b481ba912b2c103bd5a06b0ea8581968d
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-05-16 21:52:20 +0200

    Framer motion issue - fixed

--- app/page.tsx
+++ app/page.tsx
@@ -20,18 +20,20 @@ export default function Home() {
         Kelemen Hanna
       </motion.h1>
       <motion.div
-      initial={{opacity:0}}
-      animate={{opacity:1}}
+      initial={{opacity:0, x:5, y:5}}
+      animate={{opacity:1, x:0, y:0}}
       transition={{duration:1, delay: 0.2}}
+      className="relative top-0 mx-2.5 w-36 sm:w-64 md:left-40 border-2 border-purple-400 clip-path-trapezoid z-10"
       >
-        <Image src="/gallery/1.jpg" alt="" width={200} height={200} className="relative top-0 mx-2.5 w-36 sm:w-64 md:left-40 border-2 border-purple-400 clip-path-trapezoid z-10" priority={true}/>
+        <Image src="/gallery/1.jpg" alt="" width={200} height={200} priority={true}/>
       </motion.div>
       <motion.div
-      initial={{opacity:0}}
-      animate={{opacity:1}}
+      initial={{opacity:0, x:5, y:5}}
+      animate={{opacity:1, x:0, y:0}}
       transition={{duration:1, delay:0.5}}
+      className="absolute top-36 right-5 mx-2.5 w-36 sm:w-64 border-2 border-pink-600 clip-path-trapezoid"
       >
-        <Image src="/gallery/2.jpg" alt="" width={200} height={200} className="absolute top-36 right-5 mx-2.5 w-36 sm:w-64 border-2 border-pink-600 clip-path-trapezoid" priority={true}/>
+        <Image src="/gallery/2.jpg" alt="" width={200} height={200} priority={true}/>
       </motion.div>
       {language === "hun" && <h1 className="font-bold text-center py-2 text-lg italic bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-transparent">Életrajz</h1>}
       {language === "eng" && <h1 className="font-bold text-center py-2 text-lg italic bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-transparent">Biography</h1>}
