533a2bb950e891a9a78a63ab70eb1808874c29b7
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-05-29 12:06:44 +0200

    corrections

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -4,7 +4,7 @@ type Props = {}
 export default function Page({}: Props) {
 
   return (
-    <div className="bg-gradient-to-b from-white to-yellow-100/30 rounded-2xl m-2.5 p-4 flex flex-col overflow-hidden">
+    <div className="bg-gradient-to-b from-white to-yellow-100/30 rounded-2xl m-2.5 p-4 flex flex-col overflow-x-hidden">
       <div className=" flex flex-col gap-5 mt-20 items-center mb-10">
         <h1>Videos</h1>
         {videos.map((video, i) => (
