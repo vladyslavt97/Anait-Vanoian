a3a0eddaaefc22a48286d1beb079b245527e4861
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-06-14 13:30:35 +0200

    setting up the supabase

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -1,14 +1,25 @@
-import fs from 'fs';
+import supabase from "@/lib/supabase";
+
 
 export async function GET(request: Request) {
-    try {
-        
-    const arrImages = fs.readdirSync("./public/gallery/modelling")
-    console.log(arrImages.length);
-    
-    return new Response(JSON.stringify({arrImages}));
-    } catch (error) {
-        console.log('e', error);
-    }
+    const { data: files, error } = await supabase.storage.from('hanna').list("films");
+
+  if (error) {
+    console.error('Error fetching file metadata:', error);
+    return [];
+  }
+  console.log('files', files.length);  
+  
+    const publicUrls = await Promise.all(
+    files.map(async (file) => {
+      const { data: { publicUrl } } = await supabase.storage
+        .from('hanna')
+        .getPublicUrl(file.name);
+      console.log('somethin2', publicUrl);
+      return publicUrl;
+    })
+  );
+  console.log('pbu', publicUrls);
+  
 
 }
