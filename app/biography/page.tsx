ab3d1190494b3981c9bbd3b98608afa74c84b212
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-07-07 15:44:27 +0200

    bio title xl

--- app/biography/page.tsx
+++ app/biography/page.tsx
@@ -14,8 +14,8 @@ export default function Page({}: Props) {
   
   return (
     <div className="bg-pink-50 rounded-2xl m-2.5 p-4 ">
-        {language === "hun" && <h1 className="font-bold text-center py-2 text-lg italic text-black mt-10" style={{ textShadow: "1px 1px 10px rgba(255, 255, 255, 0.5)" }}>Életrajz</h1>}
-        {language === "eng" && <h1 className="font-bold text-center py-2 text-lg italic text-black mt-10" style={{ textShadow: "1px 1px 10px rgba(255, 255, 255, 0.5)" }}>Biography</h1>}
+        {language === "hun" && <h1 className="font-bold text-center py-2 text-xl text-black mt-10" style={{ textShadow: "1px 1px 10px rgba(255, 255, 255, 0.5)" }}>Életrajz</h1>}
+        {language === "eng" && <h1 className="font-bold text-center py-2 text-xl text-black mt-10" style={{ textShadow: "1px 1px 10px rgba(255, 255, 255, 0.5)" }}>Biography</h1>}
         <Bio/>
     </div>
   )
