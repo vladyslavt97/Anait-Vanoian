14d48174ddc7f6e8d04133153678fcf7c76eee49
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-07-07 15:23:15 +0200

    Hunbio updated (with old copy saved)

---
+++ components/HunBio copy.tsx
@@ -0,0 +1,117 @@
+
+type Props = {}
+
+export default function HunBio({}: Props) {
+    const divstyle = "p-2 rounded-2xl mt-5 ";
+  return (
+    <div className="sm:flex flex-col md:px-20 lg:px-44 text-blue-900">
+        2003 május 19.-én születtem Kaposváron, zenészcsaládban.
+        Zenei tanulmányaim: 2016-ig zongoráztam, 2014-ben első helyezést értem el az Országos budapesti zongoraversenyen.
+        <br/>
+        <br/>
+        2016 óta Földessy Margitot szinitanodájába járok.
+        <br/>
+        <br/>
+        <br/>
+
+
+        <span className="flex justify-center font-bold text-black" style={{ textShadow: "1px 1px 10px rgba(255, 255, 255, 0.5)" }}>Filmes szerepeim:</span> 
+        <div className={divstyle} id="thevideosdivs">
+            - Szilágyi Fanni : Orsi és Tensinhan (főszerep) 2018
+            <iframe
+                src="https://player.vimeo.com/video/456170189" frameBorder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen 
+                title="video1"
+                className="mx-auto w-64 rounded-lg"
+            />
+        </div>
+        <br/>
+        - Tóth Barnabás : Akik Maradtak (mellékszerep) 2018
+        <br/>
+        <br/>
+        <div className={divstyle} id="thevideosdivs">
+            - Szabó Szonja : Vércsék (főszerep) 2019
+            <iframe
+                src="https://player.vimeo.com/video/398000995" frameBorder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen 
+                title="video1"
+                className="mx-auto w-64 rounded-lg"
+            />
+        </div>
+
+        <div className={divstyle} id="thevideosdivs">
+            - Csukás Márton : Mire Várunk? (főszerep) 2020
+            <iframe
+                src="https://www.youtube.com/embed/j5MC0O7m4Zk" frameBorder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen 
+                title="video1"
+                className="mx-auto w-64 rounded-lg"
+            />
+        </div>
+        <br/>
+        <br/>
+
+        <span className="flex justify-center font-bold text-black" style={{ textShadow: "1px 1px 10px rgba(255, 255, 255, 0.5)" }}>Színházi szerepeim:</span> 
+        - Serei Zsolt gyermek operája - MÜPA Fesztivál Színház - “Számláló”<br/>
+        Premier : 2017 
+        <br/>
+        <br/>
+        <div className={divstyle} id="thevideosdivs">
+            - Rómeó és Júlia - Pesti Magyar Színház - Júlia
+            <iframe
+                src="https://www.youtube.com/embed/SGCUxyYyr6g" frameBorder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen 
+                title="video1"
+                className="mx-auto w-64 rounded-lg"
+            />
+        </div>
+        <br/>
+        Rendező : Eperjes Károly<br/>
+        Premier : 2020 szeptember 28
+        <br/>
+        <br/>
+        - Elfogyni az Ölelésben - Madách Színház - Csinszka<br/>
+        Rendező : Horgas Ádám<br/>
+        Várható premier : Július - augusztus
+        <br/>
+        <br/>
+        <br/>
+        - Chicago- Sopron Petőfi Színház- Roxie Hart<br/>
+        Rendező: Eperjes Károly<br/>
+        Premier: April 22nd
+        <br/>
+        <br/>
+        <br/>
+        - Veszedelmes Viszonyok<br/>
+        Cécile de Volanges<br/>
+        Rendező: Kiss Csaba<br/>
+        Premier- 2022 január 28
+        <br/>
+        <br/>
+        <br/>
+
+        <span className="flex justify-center font-bold text-black" style={{ textShadow: "1px 1px 10px rgba(255, 255, 255, 0.5)" }}>Hobbijaim: </span> 
+        - angol nyelvű irodalom (középfokú nyelvvizsga)<br/>
+        - modellkedés :<br/>
+        - 2018 - Tokio - Két hónap <br/>
+        - 2019 - Tokio - két hónap<br/>
+        Hazai lapok megjelenés : Instyle, MarieClaire, Elle
+        <br/>
+        <br/>
+        <br/>
+
+
+        <span className="flex justify-center font-bold text-black" style={{ textShadow: "1px 1px 10px rgba(255, 255, 255, 0.5)" }}>Éneklés: </span> 
+        - Bakáts Téri Iskola kórusa<br/>
+        - Kodály Zoltán Iskola kórusa<br/>
+        - Németországban - Kölni Dóm kórus - 2017-19
+        <br/>
+        <br/>
+        - Jazz/Pop dalok éneklése<br/>
+        - zongora<br/>
+        - gitár
+        <br/>
+        <br/><br/>
+    </div>
+  )
+}
--- components/HunBio.tsx
+++ components/HunBio.tsx
@@ -5,113 +5,130 @@ export default function HunBio({}: Props) {
     const divstyle = "p-2 rounded-2xl mt-5 ";
   return (
     <div className="sm:flex flex-col md:px-20 lg:px-44 text-blue-900">
-        2003 május 19.-én születtem Kaposváron, zenészcsaládban.
-        Zenei tanulmányaim: 2016-ig zongoráztam, 2014-ben első helyezést értem el az Országos budapesti zongoraversenyen.
-        <br/>
-        <br/>
-        2016 óta Földessy Margitot szinitanodájába járok.
-        <br/>
-        <br/>
-        <br/>
-
-
-        <span className="flex justify-center font-bold text-black" style={{ textShadow: "1px 1px 10px rgba(255, 255, 255, 0.5)" }}>Filmes szerepeim:</span> 
-        <div className={divstyle} id="thevideosdivs">
-            - Szilágyi Fanni : Orsi és Tensinhan (főszerep) 2018
-            <iframe
-                src="https://player.vimeo.com/video/456170189" frameBorder="0" 
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen 
-                title="video1"
-                className="mx-auto w-64"
-            />
-        </div>
-        <br/>
-        - Tóth Barnabás : Akik Maradtak (mellékszerep) 2018
-        <br/>
-        <br/>
-        <div className={divstyle} id="thevideosdivs">
-            - Szabó Szonja : Vércsék (főszerep) 2019
-            <iframe
-                src="https://player.vimeo.com/video/398000995" frameBorder="0" 
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen 
-                title="video1"
-                className="mx-auto w-64"
-            />
-        </div>
-
-        <div className={divstyle} id="thevideosdivs">
-            - Csukás Márton : Mire Várunk? (főszerep) 2020
-            <iframe
-                src="https://www.youtube.com/embed/j5MC0O7m4Zk" frameBorder="0" 
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen 
-                title="video1"
-                className="mx-auto w-64"
-            />
-        </div>
-        <br/>
-        <br/>
-
-        <span className="flex justify-center font-bold text-black" style={{ textShadow: "1px 1px 10px rgba(255, 255, 255, 0.5)" }}>Színházi szerepeim:</span> 
-        - Serei Zsolt gyermek operája - MÜPA Fesztivál Színház - “Számláló”<br/>
-        Premier : 2017 
-        <br/>
-        <br/>
-        <div className={divstyle} id="thevideosdivs">
-            - Rómeó és Júlia - Pesti Magyar Színház - Júlia
-            <iframe
-                src="https://www.youtube.com/embed/SGCUxyYyr6g" frameBorder="0" 
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen 
-                title="video1"
-                className="mx-auto w-64"
-            />
-        </div>
-        <br/>
-        Rendező : Eperjes Károly<br/>
-        Premier : 2020 szeptember 28
-        <br/>
-        <br/>
-        - Elfogyni az Ölelésben - Madách Színház - Csinszka<br/>
-        Rendező : Horgas Ádám<br/>
-        Várható premier : Július - augusztus
-        <br/>
-        <br/>
-        <br/>
-        - Chicago- Sopron Petőfi Színház- Roxie Hart<br/>
-        Rendező: Eperjes Károly<br/>
-        Premier: April 22nd
-        <br/>
-        <br/>
-        <br/>
-        - Veszedelmes Viszonyok<br/>
-        Cécile de Volanges<br/>
-        Rendező: Kiss Csaba<br/>
-        Premier- 2022 január 28
-        <br/>
-        <br/>
-        <br/>
-
-        <span className="flex justify-center font-bold text-black" style={{ textShadow: "1px 1px 10px rgba(255, 255, 255, 0.5)" }}>Hobbijaim: </span> 
-        - angol nyelvű irodalom (középfokú nyelvvizsga)<br/>
-        - modellkedés :<br/>
-        - 2018 - Tokio - Két hónap <br/>
-        - 2019 - Tokio - két hónap<br/>
-        Hazai lapok megjelenés : Instyle, MarieClaire, Elle
-        <br/>
+        A nagymúltú magyar színjátszás gyökerei nagyon mélyre nyúlnak kultúránkban. A
+        hagyományok megőrzéséhez azonban a megújulás, az új megismerése is nagyban hozzájárul.
+        Szakmai szempontból a magyar színjátszás szemléletének új viszonyba helyezése egy külföldi
+        szemléletmóddal teljesen új képet alkot a színjátszás mikéntjéről és miértjéről. Szeretném
+        felfedezni azt az utat, ami az enyém, a saját sDlsutomat megtalálni és önállóságomat és
+        önismeretemet erősíteni külföldi tanulmányaim során. Szakmai szempontból a Royal
+        Conservatoire of Scotland a világ 10 legjobb egyeteme közé került az idei évben
+        (hKps://www.rcs.ac.uk/rcs-world-top-10-qs-world-university-rankings/), így hatalmas öröm
+        és megRszteltetés, hogy felveKek és iK kezdhetem a következő szemesztert! Ehhez azonban
+        pénzügyi támogatásra is szükségem van. Családomban négyen vagyunk testvérek, akik közül
+        Gáspár (15) a világhírű londoni Menuhin School növendéke. Szüleink a közel 15 millió forintos
+        tandíját fizeRk ki éppen, így mindkeKőnk finanszírozására nincs lehetőségük a következő
+        tanév során. Úgy érzem, egy olyan lehetőség lenne a Royal Conservatoire of Scotland
+        hallgatójának lenni, amelyet nem engedhetek el. Legidősebbként felelősségemnek érzem,
+        hogy szüleimet én is segítsem azzal, hogy ösztöndíjra pályázva oktatásom teljes költsége ne
+        Őket terhelje.
         <br/>
         <br/>
 
 
-        <span className="flex justify-center font-bold text-black" style={{ textShadow: "1px 1px 10px rgba(255, 255, 255, 0.5)" }}>Éneklés: </span> 
-        - Bakáts Téri Iskola kórusa<br/>
-        - Kodály Zoltán Iskola kórusa<br/>
-        - Németországban - Kölni Dóm kórus - 2017-19
+        A megpályázoK ösztöndíjat elsősorban a magas tandíj és lakhatásom kifizetésére szeretném
+        fordítani, hisz’ az utazás és költségei még bizonyos mértékig, figyelemmel és átgondoltsággal
+        csökkenthető tétel. Az iskola magas tandíja azonban meghatározza azt, hogy egyáltalán
+        elkezdhetem-e tanulmányaimat ebben a fantaszRkus intézményben. A külföldi tapasztalaKal
+        úgy érzem, több lehetek, sokat tanulhatok és hazatérve az iKhoni színpadokon mutathatom
+        meg a közönségnek mindazt, amire a kint eltöltöK idő, energia és taníKatásom alakitoK, az a
+        rengeteg élmény, hatás és inspiráció, ami kint érhet, á^orgathatom a színpadi játékomba és
+        többet adhatok ezzel színésztársaimnak és a magyar közönségnek is.
+        A tandíj kifizetése melleK tetemes összeg a kinR megélhetés, melyben az ösztöndíj hihetetlen
+        mértékben segíteni tudna. Az egyetem melle_ munkakeresés olyan értékes energiákat öl
+        meg, melyeket fejlődésemre fordíthatnék, így tanulmányaim első évében szeretném kizárólag
+        az intézményben töltöK időre, kapcsolaKeremtésre és szakmai fejlődésre fektetni a
+        hangsúlyt! Az ösztöndíj lehetőséget teremt arra, hogy gyorsabban vethessek gyökeret az
+        elkövetkező három évben Glasgow-ban és megismerhessem a hely szellemét és közösségét,
+        beilleszkedve pedig a következő években akár már munkával együK végezve az egyetemet
+        könnyebb helyzetet létrehozva kérjem szüleim segítségét támogatásomban.
         <br/>
         <br/>
-        - Jazz/Pop dalok éneklése<br/>
-        - zongora<br/>
-        - gitár
+
+        <span className="flex justify-center font-bold text-black">Életrajz:</span>
+        Tanulmányaimat a Kodály Zoltán Ének-Zenei AMI. és Gimáziumban végeztem.
+        Magántanulóként a négy gimnáziumi évet 2 év alaK 2020-2022 közöK végeztem el. 2023-ban
+        sikeres IELTS (7.5) felsőfokú angol nyelvvizsgát teKem.
+        A színház világával korán találkoztam, legbüszkébb vagyok Eperjes Károly Rómeó és Júlia
+        adaptációjában Júlia szerepére, melyet 2022-ben mutaKunk be a PesR Magyar Színházban,
+        majd a “Veszedelmes Viszonyok” (2022, rendező: Kiss Csaba), melyben Cécile szerepébe
+        bújtam az OpereK Színházban. Csinszka (film)szerepét játszhaKam Horgas Ádám Ady
+        adaptációban: “Elfogyni az ölelésben” című filmben (2021-2022) a Madách Színházban.
+        2022-2023-ban Sztravinszkij: A Katona története színdarabban, a királykisasszony leheKem,
+        amit eljátszoKunk Magyarország 9 városának színházában s koncerKermében (mint pl.
+        Kaposvár, Veszprém, Szeged, Budapest-Zeneakadémia, Magyar Zene Háza…) s valamint
+        KolozsvároK, Marosvásárhelyen, Csíkszeredán és Székelyudvarhelyen.
+        2023-ban a Chicago Musicaljében (rendező: Eperjes Károly) Roxie szerepét játszoKam a
+        Soproni Színházban. 2020 óta számos film, sorozat, videoklip és reklám főszereplője
+        leheKem, mint pl.: “Mire várunk?” (Csukás Márton, 2020), “Karaván” c. rövidfilm (Szabó
+        Szonja, 2020), “Indulj el” (Szakács Gergő, 2021), “Hawlin”(Franciska Dalma, 2021),
+        “Haloween” (TV reklám, 2021), “Ki vagy Te?” (RTL+ csatornán, 2022 június-augusztus,
+        “Vörös” szerepe, “Majdnem Menyasszony” (Pacskovszky József, 2023 július).
+        Hálás vagyok színész tanáraim munkájának, akik ezalaK az időszak alaK segíteKek: Földessy
+        Margit, Eperjes Károly, Cserhalmi György, Salamon András, Túroczy Szabolcs és Kerekes Vica.
+        Büszke vagyok rá, hogy 2019-2022 közöK olyan színész kollégáim leheKek, mint: Molnár
+        Piroska, ix. Vidnyánszky A_la, Eperjes Károly, Bezerédi Zoltán, Haumann Máté, Trokán Anna,
+        Papadimitriu Athina, Kakassy Dóra, Botos Éva, Nagy Sándor, Soltézs Bözse, Peller Anna, Cseh,
+        Dávid Péter, Janza Kata, Kubik Petra, Kocsis Dénes, Polyák Lilla.
         <br/>
-        <br/><br/>
+        <br/>
+
+        <span className="flex justify-center font-bold text-black">Bemutatkozás:</span>
+        Kelemen Hanna vagyok, komolyzenész családba szüleKem, mindkét szülőm hegedűművész -
+        zeneakadémiai tanár, mindegyik nagyszülőm klasszikus zenész, pedagógus.
+        12 éves koromig zongoristának készültem, 13 éves koromtól Földessy Margit szinitanodájába
+        jártam, akit a mai napig tanáromnak tudhatok. 15-16 éves koromban elszerződtem az
+        AKracRve Models ügynökséghez Budapesten és sokat dolgoztam modellként.
+        Összesen 4 és fél hónapig Tokióba költöztem és oK modellként dolgoztam.
+        17 évesen volt az első premierem a PesR Magyar Színházban Júliaként, Eperjes Károly
+        rendezésében, amit 2022-ig játszoKunk.
+        Időközben Németországba, Kölnbe költöztünk családunkkal, ahol a Dóm Kórusban
+        énekeltem.
+        Ezután magántanuló leKem, hogy a 4 évnyi gimnáziumból 2 év alaK levizsgázhassak s
+        letehessem az éreKségit is.
+        Ez idő alaK még 3 premierem (ebből két musical: Chicago Sopronban és Veszedelmes
+        Viszonyok az OpereK Színházban) egy filmforgatásom egy TV sorozat forgatásom s egyéb
+        kisfilmek, videoklippek, vizsgafilmek munkáiban szerepeltem főszereplőként.
+        <br/>
+        <br/>
+
+        Ezek melleK több formációban énekeltem zenészekkel (pl. Szakcsi Lakatos RóberKel és
+        Sárközi Lajos jr-al) együKesekkel a jazz és gospel sDlusaiban. Most is több nagyszabású
+        tervvel állok idei nyári feszRválok előK újonnan alapítoK együKesekkel.
+        Egyedül gyakran kísérem magam zongorán és gitáron. Minél több hangszer elsajáDtása
+        érdekel, most épp dobolni kezdtem el. De eKől függetlenül semmi sem tesz boldogabbá,
+        mint a közös zenélés és a színjátszás.
+        A nyelvtanulás rendkívül fontos számomra. Felsőfokú angolon és magyaron kívül a
+        németemen és a franciámon is dolgozom napi szinten, hogy néhány év munka alaK szintén
+        közel anyanyelvi szinten beszélhessem őket.
+        Tervem, hogy színészként is kamatoztathassam gyarapodó nyelvtudásomat.
+        Családommal, 4 testvéremmel mai napig sok időt töltök, kisebb tesvéreimnek szüntelenül
+        próbálok belátást nyújtani a színházi világba, amiben mozgok, gyakran viszem el őket
+        főpróbáimra, előadásaimra.
+        Rendszeresen járok kiskutyámmal futni, edzeni, nagyon szeretem a sportot és izgatoK
+        vagyok, mert a skót egyetem nagy hangsúlyt fektet a mozgásra én pedig már hosszú ideje
+        tervezem még komolyabban venni a mozgáskultúrámat s az állóképességem növelését.
+        A futás számomra akárcsak egy meditáció, sokszor verseket, szövegeket, darabokat
+        memorizálok, mantrázok magamban, mialaK körbe futom a Margitszigetet.
+        A kézműves tárgyak készítése valódi hobbim! Festeni, rajzolni, nemezelni, gyöngyöt fűzni
+        mind reKentően szeretek s mindig találok új foglalkozást, amelyben alkotni tudok.
+        <br/>
+        <br/>
+
+        <span className="flex justify-center font-bold text-black">Példaképeim:</span>
+        Judy Dench, angol színész. Drámái és Shakespear-i alakításai ikonikusak, színházban és
+        filmben (Hollywood-i és művész filmekben) egyaránt intenzíven jelen van, emelleK nagyon
+        kedveli a magyar költészetet és irodalmat. Christoph Waltz, osztrák színész, azon kívül, hogy
+        különleges színész tehetség, képes a színészetében kamatoztatni nyelvi tudását. 4 nyelven is
+        játszoK már világhírű filmekben, s országát a művészetén keresztül is hűen képviselte. Stella
+        Skarsgård, Depardieu-höz hasonlóan egyszerre az egyetemes, s az ő esetében a svéd
+        kulturális örökség felemelésében s megismertetésében teK felbecsülhetetlen lépéseket. Én
+        hozzájuk hasonlóan a magyar kultúra, hagyomány és művészeR örökség megőrzése és
+        fejlesztése érdekében érzem úgy, hogy nagyon sokat fogok tudni hozzátenni a színházi világ, a
+        filmművészet, a zene, zenés színházak lemezfelvételek, videoklippek és minden ehhez tartozó
+        művészeR ágazat szenvedélyes művelésével!
+        <br/>
+        <br/>
+
     </div>
   )
 }
