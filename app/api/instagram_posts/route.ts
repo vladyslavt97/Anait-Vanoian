8cb31420bb70ce74b1283b8c202f01c36edeed3f
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-07-10 10:55:17 +0200

    spinner and tailwind

--- app/api/instagram_posts/route.ts
+++ app/api/instagram_posts/route.ts
@@ -1,8 +1,8 @@
 export async function GET(request: Request) {
-     const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_KEY}`;
+  const url = `https://graph.instagram.com/me/media?fields=id,profile_picture_url,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_KEY}`;
   const data = await fetch(url);
   const feed = await data.json();
-  console.log(feed);
+  // console.log(feed); 
   
     return new Response(JSON.stringify({feed}));
     
--- app/behind-the-scenes/page.tsx
+++ app/behind-the-scenes/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Image from "next/image";
 import { useEffect, useState } from "react";
-
+import ClipLoader from "react-spinners/ClipLoader";
 type Props = {}
 
 interface Post {
@@ -25,12 +25,17 @@ export default function Page({}: Props) {
   
   return (
     <div className="bg-pink-50 rounded-2xl m-2.5 p-4 min-h-[97vh]">
+      {posts.length === 0 ?
+      <div className="flex justify-center items-center h-96">
+        <ClipLoader color="#36d7b7" />
+      </div>
+      :  
       <div className="flex flex-col gap-10 relative top-20 mb-20">
       {posts.map(post =>{
         const isVideo = post.media_url.includes("mp4");
         
         return (
-        <div key={post.id} className="rounded-xl bg-white flex flex-col justify-center items-center">
+        <div key={post.id} className="rounded-xl bg-white max-w-[400px] flex flex-col justify-center items-center p-4">
           {!isVideo ? 
           <img src={post.media_url} alt={post.id} className="rounded-lg"/>
           : 
@@ -40,6 +45,7 @@ export default function Page({}: Props) {
           );
       })}
       </div>
+      }
     </div>
   )
 }
