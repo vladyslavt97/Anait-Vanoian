f3d37e1fa35b9244b800cb6f07ef17f28964d236
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-07-10 11:39:47 +0200

    follow and profile image

--- app/behind-the-scenes/page.tsx
+++ app/behind-the-scenes/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import ClipLoader from "react-spinners/ClipLoader";
 type Props = {}
@@ -25,6 +26,13 @@ export default function Page({}: Props) {
   
   return (
     <div className="bg-pink-50 rounded-2xl m-2.5 p-4 min-h-[97vh]">
+      <div className="relative top-12">
+        <h1>My daily life...</h1>
+        <div className="flex flex-row justify-center items-center">
+        <Image src="/avatar.jpeg" alt="avatar" width={50} height={50} className="rounded-full p-2 "/>
+        <Link href="https://www.instagram.com/hanna_kelemen_/"><button className="bg-gray-200 px-5 py-2 font-bold rounded-xl h-8 flex items-center">Follow</button></Link>
+        </div>
+      </div>
       {posts.length === 0 ?
       <div className="flex justify-center items-center h-96">
         <ClipLoader color="#36d7b7" />
@@ -39,7 +47,7 @@ export default function Page({}: Props) {
           {!isVideo ? 
           <img src={post.media_url} alt={post.id} className="rounded-lg"/>
           : 
-          <video controls src={post.media_url}/>}
+          <video controls src={post.media_url} className="rounded-xl"/>}
         <h1>{post?.caption}</h1>
         </div>
           );
