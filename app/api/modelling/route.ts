22ad53460c29bd03a29efbbde5139cfb4fabd86d
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-07-13 21:27:58 +0200

    one array with photos

--- app/api/modelling/route.ts
+++ app/api/modelling/route.ts
@@ -1,6 +1,6 @@
 import supabase from "@/lib/supabase";
 
- function extractNumberFromUrl(url: any) {
+function extractNumberFromUrl(url: any) {
   const regex = /\/(\d+)\.\w+$/;
   const match = url.match(regex);
   
@@ -19,9 +19,13 @@ function sortByNumberBeforeExtension(a:any, b:any) {
   return numberA - numberB;
 }
 
+
+
+
 export async function GET(request: Request) {
+  const publicUrls = [];
   //first under 100
-    const { data: files, error } = await supabase.storage
+    const { data: files1, error } = await supabase.storage
     .from('hanna')
     .list("modelling/modelling1");
 
@@ -31,27 +35,56 @@ export async function GET(request: Request) {
     } else {
     
     
-    const publicUrls = await Promise.all(
-    files.map(async (file) => {
+    const publicUrls1 = await Promise.all(
+    files1.map(async (file) => {
       const { data: { publicUrl } } = await supabase.storage
         .from('hanna/modelling/modelling1')
         .getPublicUrl(file.name);
-
-        
-        console.log('publicUrls:', typeof publicUrl);
         return publicUrl;
     })
   );
 
 
   //second under 100
+  let publicUrls2: string[] = [];
+  const { data: files2 } = await supabase.storage
+    .from('hanna')
+    .list("modelling/modelling2");
   
+    if (files2 !== null) {
+      publicUrls2 = await Promise.all(
+        files2.map(async (file) => {
+          const { data: { publicUrl } } = await supabase.storage
+          .from('hanna/modelling/modelling2')
+          .getPublicUrl(file.name);
+          return publicUrl;
+        })
+      );
+    } 
+    
+    //third under 100
+    let publicUrls3: string[] = [];
+    const { data: files3 } = await supabase.storage
+    .from('hanna')
+    .list("modelling/modelling3");
+    
+    if (files3 !== null) {
+      publicUrls3 = await Promise.all(
+      files3.map(async (file) => {
+        const { data: { publicUrl } } = await supabase.storage
+        .from('hanna/modelling/modelling3')
+        .getPublicUrl(file.name);
+        return publicUrl;
+      })
+      );
+    }
 
-  //third under 100
+    // sorting
+    publicUrls1.sort(sortByNumberBeforeExtension);
+    publicUrls.push(...publicUrls1)
+    publicUrls.push(...publicUrls2)
+    publicUrls.push(...publicUrls3)
 
-
-
-    const sortedUrls = publicUrls.sort(sortByNumberBeforeExtension);
     return new Response(JSON.stringify({publicUrls}));
     }
 }
