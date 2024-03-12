4e10aa62b580ba546c65a2abe861c9373ae44373
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2024-03-12 22:33:02 +0100

    films1 and 2 (55 images)

--- app/api/films/route.ts
+++ app/api/films/route.ts
@@ -2,23 +2,43 @@ import supabase from "@/lib/supabase";
 
 
 export async function GET(request: Request) {
-    const { data: files, error } = await supabase.storage
-    .from('hanna')
-    .list("films");
-
-    if (error) {
-        console.error('Error fetching file metadata:', error);
-        return [];
-    } else {
-
-    const publicUrls = await Promise.all(
-    files.map(async (file) => {
+  let publicUrls1: string[] = [];
+  const { data: files1 } = await supabase.storage
+  .from('hanna')
+  .list("films/film1");
+  
+  if (files1 !== null) {
+    publicUrls1 = await Promise.all(
+    files1.map(async (file) => {
       const { data: { publicUrl } } = await supabase.storage
-        .from('hanna/films')
+        .from('hanna/films/film1')
         .getPublicUrl(file.name);
-      return publicUrl;
-    })
-  );
-    return new Response(JSON.stringify({publicUrls}));
+        return publicUrl;
+      })
+    );
+  }
+//third under 100
+    let publicUrls2: string[] = [];
+    const { data: files2 } = await supabase.storage
+    .from('hanna')
+    .list("films/film2");
+    
+    if (files2 !== null) {
+      publicUrls2 = await Promise.all(
+      files2.map(async (file) => {
+        const { data: { publicUrl } } = await supabase.storage
+        .from('hanna/films/film2')
+        .getPublicUrl(file.name);
+        return publicUrl;
+      })
+      );
     }
+
+    //creating a new array
+    const publicUrls = [];
+    publicUrls.push(...publicUrls1)
+    publicUrls.push(...publicUrls2)
+    const filteredPublicUrls = publicUrls.filter(url => !url.endsWith('/.emptyFolderPlaceholder'));
+    console.log(filteredPublicUrls.length)
+    return new Response(JSON.stringify({publicUrls: filteredPublicUrls}));
 }
